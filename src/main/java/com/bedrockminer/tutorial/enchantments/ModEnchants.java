e2262093c0bdd78fa16efd265260bfa8d57506c6
maxb00 <maxbar00@yahoo.com>
2014-07-10 13:18:58 -0400

    Created Mod Enchantments file.

---
+++ src/main/java/com/bedrockminer/tutorial/enchantments/ModEnchants.java
@@ -0,0 +1,12 @@
+package com.bedrockminer.tutorial.enchantments;
+
+import net.minecraft.enchantment.Enchantment;
+
+public class ModEnchants {
+	
+	public static final Enchantment speedBoost = new EnchantmentSpeedBoost(84, 5);
+	
+	public static final void init(){
+		
+	}
+}
