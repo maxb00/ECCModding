cca98986d1e359100ae41b50fa05c7434724fecd
maxb00 <maxbar00@yahoo.com>
2014-07-10 13:17:45 -0400

    Main crafting file.
    This is where I register ALL of my crafting recipes.

---
+++ src/main/java/com/bedrockminer/tutorial/crafting/ModCrafting.java
@@ -0,0 +1,23 @@
+package com.bedrockminer.tutorial.crafting;
+
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
+import com.bedrockminer.tutorial.block.ModBlocks;
+import com.bedrockminer.tutorial.item.ModItems;
+import com.bedrockminer.tutorial.tools.ModTools;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public final class ModCrafting {
+
+	public static final void init() {
+
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.tutorialBlock), new Object[] {"##", "##", '#', ModItems.tutorialItem});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.tutorialItem), new Object[] {Items.redstone, new ItemStack(Items.dye, 1, 4)});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.TitaniumIng), ModItems.titaniumRod, 3);
+		GameRegistry.addRecipe(new ItemStack(ModItems.titaniumRod), new Object[] {""});
+
+	}
+
+}
