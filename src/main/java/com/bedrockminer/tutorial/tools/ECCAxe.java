4f87605d96ebce0a4e79d920fac654b3a872d89e
maxb00 <maxbar00@yahoo.com>
2014-07-10 13:28:48 -0400

    Created an Axe.

---
+++ src/main/java/com/bedrockminer/tutorial/tools/ECCAxe.java
@@ -0,0 +1,13 @@
+package com.bedrockminer.tutorial.tools;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemAxe;
+
+public class ECCAxe extends ItemAxe {
+
+	public ECCAxe(ToolMaterial eCC) {
+		super(eCC);
+	}
+
+}
