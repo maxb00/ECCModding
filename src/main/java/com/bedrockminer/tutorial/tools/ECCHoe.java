58eeb1cc9ed2fda244561833d865b9897b9800d1
maxb00 <maxbar00@yahoo.com>
2014-07-10 13:28:14 -0400

    Created a hoe.

---
+++ src/main/java/com/bedrockminer/tutorial/tools/ECCHoe.java
@@ -0,0 +1,14 @@
+package com.bedrockminer.tutorial.tools;
+
+import net.minecraft.item.ItemHoe;
+
+public class ECCHoe extends ItemHoe {
+
+	public ECCHoe(ToolMaterial e) {
+		super(e);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
