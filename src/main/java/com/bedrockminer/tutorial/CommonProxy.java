a365f05fa6e7b5dab00c12413d250b921fc481e8
maxb00 <maxbar00@yahoo.com>
2014-07-10 13:09:55 -0400

    Created Common Proxy.

---
+++ src/main/java/com/bedrockminer/tutorial/CommonProxy.java
@@ -0,0 +1,32 @@
+package com.bedrockminer.tutorial;
+
+import com.bedrockminer.tutorial.armor.SapphireItems;
+import com.bedrockminer.tutorial.block.ModBlocks;
+import com.bedrockminer.tutorial.crafting.ModCrafting;
+import com.bedrockminer.tutorial.item.ModItems;
+import com.bedrockminer.tutorial.smelting.ModSmelting;
+import com.bedrockminer.tutorial.tools.ModTools;
+
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+public class CommonProxy {
+
+    public void preInit(FMLPreInitializationEvent e) {
+        ModBlocks.init();
+        ModItems.init();
+        ModCrafting.init();
+        ModTools.init();
+        ModSmelting.init();
+        SapphireItems.init();
+    }
+
+    public void init(FMLInitializationEvent e) {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+
+    }
+}
