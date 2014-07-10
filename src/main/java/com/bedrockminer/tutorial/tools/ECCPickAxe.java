4427d4faeca116fc150757e9f8a11ee90200d774
maxb00 <maxbar00@yahoo.com>
2014-07-10 13:25:45 -0400

    Created a pickaxe.

---
+++ src/main/java/com/bedrockminer/tutorial/tools/ECCPickAxe.java
@@ -0,0 +1,15 @@
+package com.bedrockminer.tutorial.tools;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemPickaxe;
+
+public class ECCPickAxe extends ItemPickaxe {
+
+	protected ECCPickAxe(ToolMaterial pie) {
+		super(pie);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
