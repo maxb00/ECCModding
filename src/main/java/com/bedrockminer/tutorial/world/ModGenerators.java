d3d91452e1f96a60d130c18f2cfc91225fdd4f04
maxb00 <maxbar00@yahoo.com>
2014-07-11 18:11:19 -0400

    Updated ore generator.

--- src/main/java/com/bedrockminer/tutorial/world/ModGenerators.java
+++ src/main/java/com/bedrockminer/tutorial/world/ModGenerators.java
@@ -1,9 +1,5 @@
 package com.bedrockminer.tutorial.world;
 
-import net.minecraft.world.biome.BiomeGenBase;
-import net.minecraftforge.common.BiomeDictionary;
-import net.minecraftforge.common.BiomeDictionary.Type;
-import net.minecraftforge.common.BiomeManager;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ModGenerators {
